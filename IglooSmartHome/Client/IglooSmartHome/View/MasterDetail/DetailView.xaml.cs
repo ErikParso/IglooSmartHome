a3aa66ab11992250fe7a763ed68728081a45b7b3
Erik Paro <parso@kros.sk>
2019-02-13 18:06:44 +0100

    Test method returns account information.

--- IglooSmartHome/Client/IglooSmartHome/View/MasterDetail/DetailView.xaml.cs
+++ IglooSmartHome/Client/IglooSmartHome/View/MasterDetail/DetailView.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Autofac;
+using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,9 +16,26 @@ namespace IglooSmartHome.View.MasterDetail
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DetailView : ContentPage
     {
+        private readonly MobileServiceClient _client;
+
         public DetailView()
         {
             InitializeComponent();
+            _client = App.CurrentAppContainer.Resolve<MobileServiceClient>();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                lblInfo.Text = JsonConvert.SerializeObject(
+                    await _client.InvokeApiAsync("Accounts", HttpMethod.Post, new Dictionary<string, string>()),
+                    Formatting.Indented);
+            }
+            catch(Exception ex)
+            {
+                lblInfo.Text = ex.Message;
+            }
         }
     }
 }
