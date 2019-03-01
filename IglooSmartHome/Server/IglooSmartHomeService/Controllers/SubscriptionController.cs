cd58e00f8c6cda1b6824fe1e87c3aa981a776dd6
Erik Paro <parso@kros.sk>
2019-03-01 08:51:54 +0100

    Prepare controller for device subscription.

---
+++ IglooSmartHome/Server/IglooSmartHomeService/Controllers/SubscriptionController.cs
@@ -0,0 +1,30 @@
+﻿using IglooSmartHome.Models;
+using Microsoft.Azure.Mobile.Server.Config;
+using System.Web.Http;
+
+namespace IglooSmartHomeService.Controllers
+{
+    [MobileAppController]
+    public class SubscriptionController : ApiController
+    {
+        private readonly IglooSmartHomeContext _context;
+
+        public SubscriptionController(IglooSmartHomeContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            return Ok(_context.DeviceSubscriptions);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IHttpActionResult Set(string deviceId)
+        {
+            return Ok();
+        }
+    }
+}
