b32235c8da883b997708753183c5e338bc777664
Erik Parso <parsoerik@gmail.com>
2020-05-23 15:38:37 +0200

    (Client) Added test JS client. (Server) Added authentication with IglooIdentityServer authority. Configured cors for test js client.

--- IglooSmartHome/Server/Igloo.SmartHome.Server/Controller/UserController.cs
+++ IglooSmartHome/Server/Igloo.SmartHome.Server/Controller/UserController.cs
@@ -1,11 +1,13 @@
 ﻿using Igloo.SmartHome.Server.Data;
 using Igloo.SmartHome.Server.Data.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Threading.Tasks;
 
 namespace Igloo.SmartHome.Server.Controller
 {
+	[Authorize]
 	[Route("api/[controller]")]
 	[ApiController]
 	public class UserController : ControllerBase
@@ -22,15 +24,17 @@ namespace Igloo.SmartHome.Server.Controller
 		/// </summary>
 		/// <param name="name">User name.</param>
 		[HttpPost("register/{name}")]
-		public async Task RegisterUser(string name)
+		public async Task<User> RegisterUser(string name)
 		{
-			await dbContext.Users.AddAsync(new User
+			var user = new User
 			{
 				Name = name,
 				Provider = "google",
 				Sid = Guid.NewGuid().ToString()
-			});
+			};
+			await dbContext.Users.AddAsync(user);
 			await dbContext.SaveChangesAsync();
+			return await Task.FromResult(user);
 		}
 	}
 }
--- IglooSmartHome/Server/Igloo.SmartHome.Server/Startup.cs
+++ IglooSmartHome/Server/Igloo.SmartHome.Server/Startup.cs
@@ -2,7 +2,6 @@ using Igloo.SmartHome.Server.Data;
 using Igloo.SmartHome.Server.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -32,6 +31,14 @@ namespace Igloo.SmartHome.Server
 
 			services.AddControllers();
 
+			services.AddAuthentication("Bearer")
+				.AddJwtBearer("Bearer", options =>
+				{
+					options.Authority = configuration.GetConnectionString("IglooIdentityServer");
+					options.RequireHttpsMetadata = false;
+					options.Audience = "iglooSmartHomeApi";
+				});
+
 			services.AddSwaggerGen(c =>
 			{
 				c.SwaggerDoc("v1", new OpenApiInfo { Title = "Igloo Smart Home API", Version = "v1" });
@@ -40,12 +47,27 @@ namespace Igloo.SmartHome.Server
 				var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
 				c.IncludeXmlComments(xmlPath);
 			});
+
+			services.AddCors(options =>
+			{
+				// this defines a CORS policy called "default"
+				options.AddPolicy("default", policy =>
+				{
+					policy.WithOrigins("http://localhost:5003")
+						.AllowAnyHeader()
+						.AllowAnyMethod();
+				});
+			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
 			app.UpdateDatabase();
+
+			app.UseCors("default");
+
+			app.UseRouting();
 
 			app.UseSwagger();
 
@@ -59,7 +81,9 @@ namespace Igloo.SmartHome.Server
 				app.UseDeveloperExceptionPage();
 			}
 
-			app.UseRouting();
+			app.UseAuthentication();
+
+			app.UseAuthorization();
 
 			app.UseEndpoints(endpoints =>
 			{
