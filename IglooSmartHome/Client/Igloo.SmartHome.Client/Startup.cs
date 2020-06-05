bd0d524756d48ae072ba6e8488d9e5cb42f515cd
Erik Parso <parsoerik@gmail.com>
2020-06-05 16:30:34 +0200

    Renamed client project and initialized angular client.

--- IglooSmartHome/Client/Igloo.SmartHome.Client/Startup.cs
+++ IglooSmartHome/Client/Igloo.SmartHome.Client/Startup.cs
@@ -1,14 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
-namespace TestJsClient
+namespace Igloo.SmartHome.Client
 {
 	public class Startup
 	{
